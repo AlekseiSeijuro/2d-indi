80c17149352a5519ca0664b82e4f5541b94b944c
AlekseiSeijuro <141052289+AlekseiSeijuro@users.noreply.github.com>
2025-02-16 20:24:23 +0300

    add damage and hearts

--- MyRpg/Assets/scripts/Player.cs
+++ MyRpg/Assets/scripts/Player.cs
@@ -5,16 +5,22 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D body;
     private Animator anim;
+    private GameObject hpHud;
 
     private float horizontal;
     private float vertical;
     private float moveLimiter = 0.7f;
     private float runSpeed = 7.0f;
-    
+
+    private int health = 3;
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        hpHud = GameObject.Find("HP_hud");
+
+        hpHud.GetComponent<HP_hud>().addHealth(health);
     }
 
     void Update()
@@ -43,9 +49,25 @@ public class Player : MonoBehaviour
         body.linearVelocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         print("Collision detected");
     }
 
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        hpHud.GetComponent<HP_hud>().popHealth(damage);
+    }
+
+    public void takeHeal(int heal)
+    {
+        health -= heal;
+        hpHud.GetComponent<HP_hud>().addHealth(heal);
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
 }
--- MyRpg/Assets/scripts/SkeletBullet.cs
+++ MyRpg/Assets/scripts/SkeletBullet.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private Rigidbody2D body;
-    private GameObject player;
+    private GameObject playerObject;
+    private Player player;
+    private int damage = 1;
 
     private float speed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        player = GameObject.Find("Player");
+        playerObject = GameObject.Find("Player");
+        player = playerObject.GetComponent<Player>();
 
-        float difX = player.transform.position.x - transform.position.x;
-        float difY = player.transform.position.y - transform.position.y;
+        float difX = playerObject.transform.position.x - transform.position.x;
+        float difY = playerObject.transform.position.y - transform.position.y;
 
         float k = Mathf.Sqrt(Mathf.Pow(difX,2)+Mathf.Pow(difY,2))/speed; //коэфицент подобия прямоугольных треугольников, епта
 
@@ -23,6 +26,27 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
     }
 
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject collidedObject = collider.gameObject;
+
+        switch (collidedObject.name)
+        {
+            case "Player":
+                player.takeDamage(damage);
+                Destroy(this.gameObject);
+                break;
+
+            case "Skelet":
+                break;
+
+            default:
+                Destroy(this.gameObject);
+                break;
+        }
+
+    }
 }
